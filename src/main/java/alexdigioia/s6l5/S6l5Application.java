e7861e9cd0ddfdc6bfcd5395a53fb71d367e4b77
AlexDiGioia <digioia.alex2001@gmail.com>
2024-09-13 11:18:53 +0200

    Initial commit

---
+++ src/main/java/alexdigioia/s6l5/S6l5Application.java
@@ -0,0 +1,13 @@
+package alexdigioia.s6l5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S6l5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S6l5Application.class, args);
+	}
+
+}
---
+++ src/test/java/alexdigioia/s6l5/S6l5ApplicationTests.java
@@ -0,0 +1,13 @@
+package alexdigioia.s6l5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S6l5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
